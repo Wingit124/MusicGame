b1b3ccc473e69a112e1b9c7e4271c68bd8539f97
Takahashi Tsubasa <win9it.124@gmail.com>
2022-08-12 15:50:04 +0900

    Initial Commit

---
+++ MusicGame/MusicGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicGameApp.swift
+//  MusicGame
+//
+//  Created by Takahashi Tsubasa on 2022/08/12.
+//
+
+import SwiftUI
+
+@main
+struct MusicGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
